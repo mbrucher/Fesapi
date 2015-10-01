d62e06c48f654720ba26ad55efa1c8faa7a3b561
philippe.verney <philippe.verney@553bd026-d4ad-425d-a02a-e125c7656c9a>
2015-10-01 08:48:12 +0000

    Example now serializes and deserializes back.
    git-svn-id: http://forge2.pole-aquinetic.fr/svnroot/fesapi/trunk@147 553bd026-d4ad-425d-a02a-e125c7656c9a

--- example/example.cpp
+++ example/example.cpp
@@ -1559,12 +1559,12 @@ int main(int argc, char **argv)
 */
 
 // filepath is defined in a macro to better check memory leak
-//#define filePath "../../testingPackageCpp.epc"
+#define filePath "../../testingPackageCpp.epc"
 //#define filePath "C:/Users/Philippe/data/resqml/resqmlExchangedModel/v2_0/paradigm/unstructured.epc"
-#define filePath "C:/Users/Philippe/data/resqml/resqmlExchangedModel/v2_0/cvx/fully_unstructured_mesh/2014-08-22/house.epc"
+//#define filePath "C:/Users/Philippe/data/resqml/resqmlExchangedModel/v2_0/cvx/fully_unstructured_mesh/2014-08-22/house.epc"
 int main(int argc, char **argv)
 {
-	//serialize(filePath);
+	serialize(filePath);
 	deserialize(filePath);
 
 	cout << "Press enter to continue..." << endl;
