0ff6012c6229a4f0e645f49abb7d5b64537ab587
philippe.verney <philippe.verney@553bd026-d4ad-425d-a02a-e125c7656c9a>
2015-11-11 19:29:32 +0000

    * Upgrade the cs project to .net framework 4.5.1 instead of 4.5 * Improvement of the findSZIP module for cmake in order to show include dir and library variables in cmake GUI.
    git-svn-id: http://forge2.pole-aquinetic.fr/svnroot/fesapi/trunk@174 553bd026-d4ad-425d-a02a-e125c7656c9a

--- cmake/modules/FindSZIP.cmake
+++ cmake/modules/FindSZIP.cmake
@@ -29,6 +29,13 @@ if(SZIP_ROOT)
   list(APPEND _SZIP_SEARCHES _SZIP_SEARCH_ROOT)
 endif()
 
+# Normal search.
+set(_SZIP_SEARCH_NORMAL
+  PATHS "[HKEY_LOCAL_MACHINE\\SOFTWARE\\GnuWin32\\szip;InstallPath]"
+        "$ENV{PROGRAMFILES}/szip"
+  )
+list(APPEND _SZIP_SEARCHES _SZIP_SEARCH_NORMAL)
+
 # Try each search configuration.
 foreach(search ${_SZIP_SEARCHES})
   find_path(SZIP_INCLUDE_DIR NAMES szlib.h ${${search}} PATH_SUFFIXES include)
@@ -36,7 +43,7 @@ foreach(search ${_SZIP_SEARCHES})
   find_library(SZIP_LIBRARY_STATIC  NAMES libszip ${${search}} PATH_SUFFIXES lib)
 endforeach()
 
-mark_as_advanced(SZIP_LIBRARY SZIP_INCLUDE_DIR)
+mark_as_advanced(SZIP_LIBRARY_STATIC SZIP_LIBRARY_SHARED SZIP_INCLUDE_DIR)
 
 if(SZIP_INCLUDE_DIR AND EXISTS "${SZIP_INCLUDE_DIR}/szlib.h")
     file(STRINGS "${SZIP_INCLUDE_DIR}/szlib.h" SZIP_H REGEX "^#define SZLIB_VERSION \"[^\"]*\"$")
@@ -48,7 +55,6 @@ endif()
 
 # handle the QUIETLY and REQUIRED arguments and set SZIP_FOUND to TRUE if
 # all listed variables are TRUE
-#include(${CMAKE_ROOT}/share/cmake-${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}/Modules/FindPackageHandleStandardArgs.cmake)
 include(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
 if (SZIP_LIBRARY_SHARED)
 	FIND_PACKAGE_HANDLE_STANDARD_ARGS(SZIP REQUIRED_VARS SZIP_LIBRARY_SHARED SZIP_INCLUDE_DIR)
