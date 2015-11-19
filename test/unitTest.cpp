4cffa5b387632b378c761d9a810e5ac1ebd84ea4
philippe.verney <philippe.verney@553bd026-d4ad-425d-a02a-e125c7656c9a>
2015-11-19 10:20:43 +0000

    Use of a simple macro for defining standard unit tests.
    git-svn-id: http://forge2.pole-aquinetic.fr/svnroot/fesapi/trunk@179 553bd026-d4ad-425d-a02a-e125c7656c9a

--- test/unitTest.cpp
+++ test/unitTest.cpp
@@ -24,139 +24,48 @@
 #include "resqml2_0_1test/ContinuousPropertyOnPartialGridTest.h"
 #include "resqml2_0_1test/WellboreMarkerFrameRepresentationTest.h"
 
-#include "EpcDocument.h"
-#include "resqml2_0_1/LocalDepth3dCrs.h"
-#include "resqml2_0_1/LocalTime3dCrs.h"
-#include "resqml2_0_1/FaultInterpretation.h"
-#include "resqml2_0_1/HdfProxy.h"
-#include "resqml2_0_1/WellboreTrajectoryRepresentation.h"
-#include "resqml2_0_1/MdDatum.h"
-#include "resqml2_0_1/WellboreMarkerFrameRepresentation.h"
-#include "resqml2_0_1/SubRepresentation.h"
-#include "resqml2_0_1/StratigraphicColumn.h"
-#include "resqml2_0_1/StratigraphicColumnRankInterpretation.h"
-#include "resqml2_0_1/StratigraphicOccurrenceInterpretation.h"
-#include "resqml2_0_1/StratigraphicUnitFeature.h"
-#include "resqml2_0_1/StratigraphicUnitInterpretation.h"
-#include "resqml2_0_1/WellboreInterpretation.h"
-
 using namespace commontest;
 using namespace resqml2_0_1test;
 
+#define FESAPI_TEST(name, tags, classTest)  TEST_CASE(name, tags)\
+{\
+	classTest* test = new classTest("../../" #classTest ".epc");\
+	test->serialize();\
+	test->deserialize();\
+	delete test;\
+}
+
 TEST_CASE( "Deserialize an EPC document", "[epc]")
 {
-	EpcDocumentTest* epcDocumentTest = new EpcDocumentTest("../../testingPackageCpp.epc");
-	epcDocumentTest->deserialize();
+	EpcDocumentTest* test = new EpcDocumentTest("../../testingPackageCpp.epc");
+	test->deserialize();
+	delete test;
 }
 
-TEST_CASE( "Export and import a local depth 3d crs", "[crs]" )
-{
-	LocalDepth3dCrsTest* localDepth3dCrsTest = new LocalDepth3dCrsTest("../../localDepth3dCrsTest.epc");
+FESAPI_TEST("Export and import a local depth 3d crs", "[crs]", LocalDepth3dCrsTest)
 
-	localDepth3dCrsTest->serialize();
-	localDepth3dCrsTest->deserialize();
-}
+FESAPI_TEST("Export and import an horizon", "[feature]", HorizonTest)
 
-TEST_CASE( "Export and import an horizon", "[feature]" )
-{
-	HorizonTest* horizonTest = new HorizonTest("../../testHorizon.epc");
-	
-	horizonTest->serialize();
-	horizonTest->deserialize();
-}
+FESAPI_TEST("Export and import an horizon interpretation", "[interpretation]", HorizonInterpretationTest)
 
-TEST_CASE( "Export and import an horizon interpretation", "[interpretation]" )
-{
-	HorizonInterpretationTest* horizonInterpretationTest = new HorizonInterpretationTest("../../testHorizonInterpretation.epc");
+FESAPI_TEST("Export and import a single patch triangulated set fault representation", "[representation]", FaultSinglePatchTriangulatedSetRepresentationTest)
 
-	horizonInterpretationTest->serialize();
-	horizonInterpretationTest->deserialize();
-}
+FESAPI_TEST("Export and import a multi patch triangulated set fault representation", "[representation]", FaultMultiPatchTriangulatedSetRepresentationTest)
 
-TEST_CASE( "Export and import a single patch triangulated set fault representation", "[representation]" )
-{
-	FaultSinglePatchTriangulatedSetRepresentationTest* faultSinglePatchTriangulatedSetRepresentation = new FaultSinglePatchTriangulatedSetRepresentationTest("../../faultSinglePatchTriangulatedSetRepresentation.epc");
-	
-	faultSinglePatchTriangulatedSetRepresentation->serialize();
-	faultSinglePatchTriangulatedSetRepresentation->deserialize();
-}
+FESAPI_TEST("Export and import a single and multi patch triangulated set fault representation", "[representation]", FaultSingleAndMultiPatchTriangulatedSetRepresentationTest)
 
-TEST_CASE( "Export and import a multi patch triangulated set fault representation", "[representation]" )
-{
-	FaultMultiPatchTriangulatedSetRepresentationTest* faultMultiPatchTriangulatedSetRepresentation = new FaultMultiPatchTriangulatedSetRepresentationTest("../../faultMultiPatchTriangulatedSetRepresentation.epc");
-	
-	faultMultiPatchTriangulatedSetRepresentation->serialize();
-	faultMultiPatchTriangulatedSetRepresentation->deserialize();
-}
+FESAPI_TEST("Export and import a generic creation activity template", "[activity]", ActivityTemplateGenericCreationTest)
 
-TEST_CASE( "Export and import a single and multi patch triangulated set fault representation", "[representation]" )
-{
-	FaultSingleAndMultiPatchTriangulatedSetRepresentationTest* faultSingleAndMultiPatchTriangulatedSetRepresentationTest = new FaultSingleAndMultiPatchTriangulatedSetRepresentationTest("../../faultSingleAndMultiPatchTriangulatedSetRepresentation.epc");
-	
-	faultSingleAndMultiPatchTriangulatedSetRepresentationTest->serialize();
-	faultSingleAndMultiPatchTriangulatedSetRepresentationTest->deserialize();
-}
+FESAPI_TEST("Export and import an activity", "[activity]", ActivityCreationTest)
 
-TEST_CASE( "Export and import a generic creation activity template", "[activity]" )
-{
-	ActivityTemplateGenericCreationTest* activityTemplate = new ActivityTemplateGenericCreationTest("../../activityTemplateGenericCreationTest.epc");
+FESAPI_TEST("Export and import an unstructured grid", "[grid]", OneTetrahedronUnstructuredGridRepresentationTest)
 
-	activityTemplate->serialize();
-	activityTemplate->deserialize();
-}
+FESAPI_TEST("Ijk to unstructured grid", "[grid]", UnstructuredFromIjkGridRepresentationTest)
 
-TEST_CASE( "Export and import an activity", "[activity]" )
-{
-	ActivityCreationTest* activity = new ActivityCreationTest("../../activityCreationTest.epc");
+FESAPI_TEST("Export and import a time series", "[property]", TimeSeriesTest)
 
-	activity->serialize();
-	activity->deserialize();
-}
+FESAPI_TEST("Export and import continuous property series", "[property]", ContinuousPropertySeriesTest)
 
-TEST_CASE( "Export and import an unstructured grid", "[grid]" )
-{
-	OneTetrahedronUnstructuredGridRepresentationTest* gridTest = new OneTetrahedronUnstructuredGridRepresentationTest("../../oneTetrahedronUnstructuredGridRepresentationTest.epc");
+FESAPI_TEST("Export and import continuous property on partial grid", "[property]", ContinuousPropertyOnPartialGridTest)
 
-	gridTest->serialize();
-	gridTest->deserialize();
-}
-
-TEST_CASE( "Ijk to unstructured grid", "[grid]" )
-{
-	UnstructuredFromIjkGridRepresentationTest* gridTest = new UnstructuredFromIjkGridRepresentationTest("../../unstructuredFromIjkGridRepresentationTest.epc");
-
-	gridTest->serialize();
-	gridTest->deserialize();
-}
-
-TEST_CASE( "Export and import a time series", "[property]" )
-{
-	TimeSeriesTest* timeSeriesTest = new TimeSeriesTest("../../timeSeriesTest.epc");
-
-	timeSeriesTest->serialize();
-	timeSeriesTest->deserialize();
-}
-
-TEST_CASE( "Export and import continuous property series", "[property]" )
-{
-	ContinuousPropertySeriesTest* continuousPropertySeriesTest = new ContinuousPropertySeriesTest("../../continuousPropertySeriesTest.epc");
-
-	continuousPropertySeriesTest->serialize();
-	continuousPropertySeriesTest->deserialize();
-}
-
-TEST_CASE( "Export and import continuous property on partial grid", "[property]" )
-{
-	ContinuousPropertyOnPartialGridTest* continuousPropertyOnPartialGridTest = new ContinuousPropertyOnPartialGridTest("../../continuousPropertyOnPartialGridTest.epc");
-
-	continuousPropertyOnPartialGridTest->serialize();
-	continuousPropertyOnPartialGridTest->deserialize();
-}
-
-TEST_CASE("Export and import a wellbore marker frame", "[well][stratigraphy]")
-{
-	WellboreMarkerFrameRepresentationTest* wellboreMarkerFrameRepresentationTest = new WellboreMarkerFrameRepresentationTest("../../wellboreMarkerFrameRepresentationTest.epc");
-
-	wellboreMarkerFrameRepresentationTest->serialize();
-	wellboreMarkerFrameRepresentationTest->deserialize();
-}
+FESAPI_TEST("Export and import a wellbore marker frame", "[well][stratigraphy]", WellboreMarkerFrameRepresentationTest)
